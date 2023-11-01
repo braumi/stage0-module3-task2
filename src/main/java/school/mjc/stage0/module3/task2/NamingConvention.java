dfd062485249928e0e394cc43d064f8a009387bb
braumi <dalalishviligiorgi36@gmail.com>
2023-11-01 14:14:52 +0400

    euf

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
+        int variableZero = 0;
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
@@ -10,5 +11,6 @@ public class NamingConvention {
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
+        int variableNine = 9;
     }
 }
